83f9a2f2b4686d06c799ef82c1f4fed03cfc5615
lubenard <lubenard@student.42.fr>
2021-09-30 18:53:35 +0200

    Improved free + added show_alloc_mem_ex in header

--- srcs/free.c
+++ srcs/free.c
@@ -6,7 +6,7 @@
 /*   By: lubenard <lubenard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/28 11:21:03 by lubenard          #+#    #+#             */
-/*   Updated: 2021/09/28 18:44:40 by lubenard         ###   ########.fr       */
+/*   Updated: 2021/09/30 17:58:07 by lubenard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,31 +20,20 @@ extern pthread_mutex_t g_mutex;
 // TODO: Rework this function
 void merge_blocks(t_alloc *node_ptr) {
 	t_alloc *node_tmp;
-	t_alloc *node_tmp_2;
 
 	node_tmp = node_ptr;
 	printk("Merge block, actually on %p\n", node_tmp);
 	while (node_tmp->prev) {
-		//printk("Reversing linked list : Actually on %p, going on %p\n", node_ptr, node_tmp->prev);
+		//printk("(Bloc merge) Reversing linked list : Actually on %p, going on %p\n", node_ptr, node_tmp->prev);
 		node_tmp = node_tmp->prev;
 	}
 	printk("Current merge pointer is %p\n", node_tmp);
 	while (node_tmp->next) {
-		if (node_tmp->is_busy == 0) {
-			break;
-		}
-		node_tmp = node_tmp->next;
-	}
-	node_tmp_2 = node_tmp;
-	node_tmp = node_tmp->next;
-	while (node_tmp->next) {
-		if (node_tmp->is_busy) {
-			node_tmp_2->size += node_tmp->size;
-			printk("Merging %p with %p, for total size of %lu\n", node_tmp_2, node_tmp, node_tmp_2->size);
-		}
+		printk("(Bloc merge) Going on linked list : Actually on %p, going on %p\n", node_ptr, node_tmp->next);
 		node_tmp = node_tmp->next;
 	}
 }
+
 
 void	free(void *ptr) {
 	t_alloc *node_ptr;
@@ -54,12 +43,13 @@ void	free(void *ptr) {
 	pthread_mutex_lock(&g_mutex);
 	printk("---------REQUESTING FREE------------\n");
 	printk("Getting %p from arg\n", ptr);
-	//printk("Pointer should be at %p\n", g_curr_node->prev);
 	node_ptr = (t_alloc *)((char *) ptr - sizeof(t_alloc) - 1);
 	if (node_ptr->buffer_overflow == MAGIC_NUMBER) {
 		printk("Freeing from address %p\n", node_ptr);
 		node_ptr->is_busy = 1;
-		//merge_blocks(node_ptr);
+		/*if ((node_ptr->prev && node_ptr->prev->is_busy == 1) ||
+			(node_ptr->next && node_ptr->next->is_busy == 1))
+			merge_blocks(node_ptr);*/
 		//munmap(node_ptr, sizeof(t_alloc) + node_ptr->size);
 	}
 	printk("----------END FREE---------------\n");
